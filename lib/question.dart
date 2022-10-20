925ddfed4e9bdcb9005ee3e1ed3df9524d1778ed
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-10-21 02:44:33 +0500

    .

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,7 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.all(10),
       width: double.infinity,
       child: Text(
         questionText,
