b0c7a70acaaafb20ba613d4118ce033c79869215
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-10-21 02:20:45 +0500

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
